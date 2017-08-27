ddbbdd7fa8b923f369f5d62494fa698063c1fa7a
Pedro Mendonça <pgcalixto@gmail.com>
2017-08-27 08:53:53 -0300

    Added test regarding the WrongSizeList exception
    Now there is a test for the custom exception.
    Also, the previous implemented test when clause is no more hard-coded.

--- test/main.feature
+++ test/main.feature
@@ -4,3 +4,7 @@ Feature: testing the main application
      Given we have a player guess and a game answer
       when we test it against the answer
       then we have the correct and regular count
+
+     Given we have a player guess with an incorrect size
+      when we test it against the answer
+      then it throws a WrongSizeList exception
--- test/steps/main.py
+++ test/steps/main.py
@@ -9,9 +9,21 @@ def step_impl(context):
 
 @when('we test it against the answer')
 def step_impl(context):
-    # context.element_count = main.check_answer(context.guess, context.answer)
-    context.element_count = main.check_answer([5, 3, 2, 1], [1, 2, 3, 4])
+    try:
+        context.element_count = main.check_answer(context.guess, context.answer)
+        context.exception = None
+    except Exception as e:
+        context.exception = e
 
 @then('we have the correct and regular count')
 def step_impl(context):
     assert context.element_count == (0, 3)
+
+@given('we have a player guess with an incorrect size')
+def step_impl(context):
+    context.guess = [5, 3, 2]
+    context.answer = [1, 2, 3, 4]
+
+@then('it throws a WrongSizeList exception')
+def step_impl(context):
+    assert isinstance(context.exception, main.WrongSizeList)
